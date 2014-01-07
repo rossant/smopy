df8dbb63907242b258127aadfa615b678c37eae4
Cyrille Rossant <cyrille.rossant@gmail.com>
2014-01-07 22:03:07 +0100

    Added setup.py.

---
+++ setup.py
@@ -0,0 +1,25 @@
+import os
+from setuptools import setup
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name="smopy",
+    version="0.0.1",
+    author="Cyrille Rossant",
+    author_email="rossant@github",
+    description=(("OpenStreetMap image tiles in Python")),
+    license="BSD",
+    keywords="openstreetmap matplotlib map maps ipython",
+    url="http://packages.python.org/smopy",
+    py_modules=['smopy'],
+    long_description=read('README.md'),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Utilities",
+        "Framework :: IPython",
+        "License :: OSI Approved :: BSD License",
+    ],
+)
+
